18363dbd0f7ad94cb91fb1c897d5d40772d62d81
AndyVuma <78667634+AndyVuma@users.noreply.github.com>
2023-02-22 13:14:24 +0200

    Login Page
    This Login page was created using HTML, CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#rememberMe").click(function() {
+      if ($(this).is(":checked")) {
+        // Code to remember the user
+      } else {
+        // Code to forget the user
+      }
+    });
+  });
+  
